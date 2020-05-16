2617ce568eea0052a1d763f334580c94346066b9
sai6855 <60743144+sai6855@users.noreply.github.com>
2020-05-16 11:16:53 +0530

    Updates

---
+++ precache-manifest.b9f2c90e19b5914af87ced6b770a5969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8a333ed822bd65c1dbbb1004367c3e",
+    "url": "/corona-live-track/index.html"
+  },
+  {
+    "revision": "ee5e71b452927ff47b15",
+    "url": "/corona-live-track/static/css/main.93e8fadc.chunk.css"
+  },
+  {
+    "revision": "73e5713d700d17dd7d3e",
+    "url": "/corona-live-track/static/js/2.092e4f22.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-live-track/static/js/2.092e4f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5e71b452927ff47b15",
+    "url": "/corona-live-track/static/js/main.27e03bef.chunk.js"
+  },
+  {
+    "revision": "8656b62972a8a7366996",
+    "url": "/corona-live-track/static/js/runtime-main.d91fabca.js"
+  }
+]);
